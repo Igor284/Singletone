b01a65295006e48e6c3a8cfd7ac058257c3b2cbd
Igor284 <kochat.lgor@gmail.com>
2021-09-05 14:06:25 +0300

    first commit

---
+++ Singletone/FileWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    public sealed class FileWorker
+    {
+        private static readonly Lazy<FileWorker> instance = new Lazy<FileWorker>(() => new FileWorker());
+
+        public static FileWorker Instance { get { return instance.Value; } }
+       
+        public string FilePath { get; }
+
+        public string Text { get; private set; }
+
+
+        private  FileWorker()
+        {
+            FilePath = "text.txt";
+            ReadTextFromFile();
+        }
+
+        public void WhriteText(string text)
+        {
+            Text += text;
+        }
+
+        public void Save()
+        {
+            using(var writer = new StreamWriter(FilePath, false))
+            {
+                writer.WriteLine(Text);
+            }
+        }
+        private void ReadTextFromFile()
+        {
+            if (!File.Exists(FilePath))
+            {
+                Text = "";
+            }
+            else
+            {
+                using(var reader = new StreamReader(FilePath))
+                {
+                    Text = reader.ReadToEnd();
+                }
+            }
+        }
+
+    }
+}
---
+++ Singletone/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var text1 = new FileWorker();
+            var text2 = new FileWorker();*/
+
+            var text1 = FileWorker.Instance;
+            var text2 = FileWorker.Instance;
+
+            text1.WhriteText("hhh");
+            text2.WhriteText("ggg");
+            
+            text1.Save();
+            text2.Save();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
